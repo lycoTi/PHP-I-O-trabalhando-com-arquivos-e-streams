3a4611a09eb64baaad43f943325b782754367317
Tiago <tiagosantos__@live.com>
2022-04-12 11:37:44 -0400

    facilitando a escrita

--- escritor-cursos.php
+++ escritor-cursos.php
@@ -1,9 +1,5 @@
 <?php
 
-$arquivo = fopen("cursos-php.txt", "a");
+$curso = "\nDesign Patterns em PHP: padrões comportamentais";
 
-$curso = "\nDesign Patterns em PHP: padrões estruturais";
-
-fwrite($arquivo, $curso);
-
-fclose($arquivo);
+file_put_contents("cursos-php.txt", $curso, FILE_APPEND);
